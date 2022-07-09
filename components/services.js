cfc99889fe6c484ab9831f1a3d4a3733ea71de40
Eric Turner <eric@ericturner.dev>
2022-07-09 18:37:44 -0400

    Add browser extension development to services

--- components/services.js
+++ components/services.js
@@ -8,19 +8,24 @@ const servicesData = [
         description: "Everything from basic websites to complex web applications."
     },
     {
-        id: "api-development",
-        name: "API Development",
-        description: "Develop performant, scalable REST APIs."
-    },
-    {
         id: "database-management",
         name: "Database Management",
         description: "Design, modify, and query PostgreSQL and Mongodb databases."
     },
     {
+        id: "api-development",
+        name: "API Development",
+        description: "Develop performant, scalable REST APIs."
+    },
+    {
         id: "cloud-management",
         name: "Cloud Management",
         description: "Set up and maintain AWS cloud environments."
+    },
+    {
+        id: "browser-extensions",
+        name: "Browser Extension Development",
+        description: "Develop add ons for Chrome and Firefox."
     },
     {
         id: "linux-administration",
@@ -50,8 +55,8 @@ export default function Services({className}) {
     return (
         <section className={classNames('services flex flex-col md:items-center', className)}>
             <div className="flex flex-col md:flex-row px-4 max-w-4xl">
-                {partition(servicesData,2).map((services, i) => (
-                    <ServiceGroup key={`group-${i}`} className={`flex flex-col md:w-1/3`} services={services} />
+                {partition(servicesData, 2).map((services, i) => (
+                    <ServiceGroup key={`group-${i}`} className={`flex flex-col md:w-1/3`} services={services}/>
                 ))}
             </div>
         </section>
