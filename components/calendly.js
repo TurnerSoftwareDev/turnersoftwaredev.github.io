47e8667a42e5153c6faf9340a8efd21d34b07f4a
Eric Turner <eric@ericturner.dev>
2022-07-09 19:50:05 -0400

    Add Calendly integration

---
+++ components/calendly.js
@@ -0,0 +1,21 @@
+import Script from 'next/script';
+import classNames from 'classnames';
+
+export default function Calendly({className}) {
+    return (
+        <section className={classNames('consultation flex flex-col w-full items-center p-4 md:p-8', className)}>
+            <div className="flex flex-col md:flex-row justify-center max-w-6xl ">
+                <div className="flex flex-col md:w-1/3">
+                    <h2>Schedule a Consultation</h2>
+                    <p className="font-semibold">I am available for a consultation if you need advice about the software challenges your organization faces.</p>
+                    <p>Duration: 30 minutes</p>
+                    <p>Cost: $50 (invoiced upon completion)</p>
+                </div>
+
+                <div className="calendly-inline-widget min-w-[320px] h-[650px]"
+                     data-url="https://calendly.com/turnersoftwaredevelopment/consultation?hide_event_type_details=1&hide_gdpr_banner=1"/>
+            </div>
+            <Script src="https://assets.calendly.com/assets/external/widget.js"/>
+        </section>
+    )
+}
--- components/pages/home.js
+++ components/pages/home.js
@@ -1,18 +1,16 @@
 import About from '../about';
-import Contact from '../contact';
 import HeroUnit from '../heroUnit';
 import PageLayout from '../pageLayout';
-import Portfolio from '../portfolio';
 import Services from '../services';
+import Calendly from '../calendly';
 
 export default function HomePage() {
     return (
         <PageLayout title="Home - Turner Software Development">
             <HeroUnit className="mb-2"/>
             <Services className="mb-2" />
-            <About className="bg-slate-200"/>
-            {/*<Portfolio className="mb-2" />*/}
-            {/*<Contact className="mb-2" />*/}
+            <About className="mb-2 bg-slate-200"/>
+            <Calendly />
         </PageLayout>
     )
 }
