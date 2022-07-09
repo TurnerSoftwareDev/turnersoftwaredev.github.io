07f3318b7d551ff146928a5169744e3299bc5817
Eric Turner <eric@ericturner.dev>
2022-07-09 14:41:11 -0400

    Add a word

--- components/about.js
+++ components/about.js
@@ -12,7 +12,7 @@ export default function About({className}) {
                        height={466/3} />
 
                 <p className="ml-4 w-5/6">Eric Turner has {new Date().getFullYear()-1998} years of software development
-                    and IT experience, has worked in defense, intelligence, and publishing industries, holds B.S. Computer Science and B.A. Spanish degrees from Western Washington University,
+                    and IT experience, has worked in the defense, intelligence, and publishing industries, holds B.S. Computer Science and B.A. Spanish degrees from Western Washington University,
                 and lives near Annapolis, Maryland.</p>
             </div>
         </section>
